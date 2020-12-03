0cab75deb918295ba28bee1a2cf7464dc4cf9246
Chris Connor <chris@chrisconnor.co.uk>
2020-12-03 08:29:49 +0000

    Added a regex version

--- src/uk/co/chrisconnor/aoc/DayTwo.java
+++ src/uk/co/chrisconnor/aoc/DayTwo.java
@@ -1,7 +1,11 @@
 package uk.co.chrisconnor.aoc;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DayTwo {
 
@@ -11,18 +15,20 @@ public class DayTwo {
         this.inputList = Helper.getStringsFromFile(filename);
     }
 
+
+
     public int getPartOneAnswer(){
 
         List<String> validPasswords = new ArrayList<>();
 
         inputList.forEach( iLine -> {
 
-            String[] components = iLine.split(" ");
+            Map<String, String> properties = extractProperties(iLine);
 
-            int min = Integer.parseInt(components[0].split("-")[0]);
-            int max = Integer.parseInt(components[0].split("-")[1]);
-            char letter = components[1].split(":")[0].charAt(0);
-            String password = components[2];
+            int min = Integer.parseInt(properties.get("min"));
+            int max = Integer.parseInt(properties.get("max"));
+            char letter = properties.get("letter").charAt(0);
+            String password = properties.get("password");
 
             int charCount = 0;
             for(int i = 0; i < password.length(); i++){
@@ -41,6 +47,26 @@ public class DayTwo {
         return validPasswords.size();
 
     }
+
+    private Map<String, String> extractProperties(String input){
+
+        // 2-5 b: sbgqtqcwrkxbqbq
+        String regexPattern = "(\\d+)\\-(\\d+)\\s([A-Za-z])\\:\\s(.*)";
+        Pattern p = Pattern.compile(regexPattern);
+
+        Matcher m = p.matcher(input);
+        Map<String, String> result = new HashMap<>();
+        if(m.find()){
+            result.put("min", m.group(1));
+            result.put("max",m.group(2));
+            result.put("letter",m.group(3));
+            result.put("password",m.group(4));
+        }
+
+        return result;
+
+    }
+
 
     public int getPartTwoAnswer(){
 
